0e326407b295ece424998811b3732f7c68f1dc29
github username <souzajohnlee@gmail.com>
2020-10-13 20:57:01 -0700

    create welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,11 @@
 class UserMailer < ApplicationMailer
+    default from: 'notifications@example.come'
+
+
+    def welcome_email
+        @user = params[:user]
+        @url = 'http://example.com/login'
+        mail(to: @user.email, subject: 'Welcome to my awesome site')
+    end
+
 end
